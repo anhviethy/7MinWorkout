e5e0d955e198027fa82e6cda4b6dfa5d1ff032ab
via <anhviethy@gmail.com>
2023-02-23 22:37:58 +0700

    [22:37 - 23/02/2023] Setting up main UI

--- app/src/main/java/com/via/a7minworkout/MainActivity.kt
+++ app/src/main/java/com/via/a7minworkout/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.via.a7minworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.via.a7minworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding.flStart.setOnClickListener {
+            Toast.makeText(this, "Start clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
