2861140849d50c9be3caaf006962c1ff85ca86a1
via <anhviethy@gmail.com>
2023-02-28 21:28:44 +0700

    [21:27 - 28/02/2023] Custom progressbar to a ring then use countdown timer to count time to resting

--- app/src/main/java/com/via/a7minworkout/ExerciseActivity.kt
+++ app/src/main/java/com/via/a7minworkout/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package com.via.a7minworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.via.a7minworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +20,55 @@ class ExerciseActivity : AppCompatActivity() {
 
         setSupportActionBar(binding?.toolBarExercise)
 
-        if(supportActionBar != null){
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
 
         binding?.toolBarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setUpRestView()
+    }
+
+    private fun setUpRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(remainingTime: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Now we will start the exercise",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        binding = null
     }
 }
